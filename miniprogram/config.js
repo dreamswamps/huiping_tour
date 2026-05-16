34b8d08e383a21be7b997a4b31221a5957139926
WeIR\Xqc <weiranxiaobai@outlook.com>
2026-05-16 23:37:15 +0800

    HTTPS配置

--- miniprogram/config.js
+++ miniprogram/config.js
@@ -5,7 +5,8 @@
 module.exports = {
   // 须与后端监听端口一致（server/app.js：process.env.PORT || 3000）
   // 若你在 .env 里写了 PORT=5000，这里要改成 http://localhost:5000
-  baseUrl: 'http://110.40.187.34:3000',
+  // baseUrl: 'http://110.40.187.34:3000',
+  baseUrl: 'https://www.weir123456.cloud',
 
   // 腾讯地图插件 Key（lbs 服务）
   qqMapKey: '37OBZ-T3YKC-ZOQ24-AQEFG-QJ3ST-ADFAU',
